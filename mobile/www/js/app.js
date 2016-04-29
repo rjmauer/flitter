e8cfb61220b5ad449b6edd7332eab8eef54d2d12
Berett Babrich <bebabrich@ursinus.edu>
2016-04-28 20:05:35 -0400

    added addFriend form page

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -113,6 +113,16 @@ angular.module('starter', ['ionic',  'starter.controllers', 'starter.services', 
   }
 })
 
+.state('app.addFriend', {
+  url: '/addFriend',
+  views: {
+	'menuContent': {
+	  templateUrl: 'templates/addFriend.html',
+	  controller: 'EventCtrl'
+	}
+  }
+})
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/login');
 
